39c78851741ddea36cc14209a9e2eba5e6ba3ad4
Gilberto Garcia <GGG12022021@gmail.com>
2022-01-20 20:19:15 -0600

    Add simple UI

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-game();
+//game();
+let win=0;
+let lose=0;
+let tie=0;
 
-function game(){
+let buttons = Array.from(document.querySelectorAll("button"));
+
+for(let button of buttons){
+    button.addEventListener('click',playRound);
+}
+
+
+
+
+function computerPlay(){
+    const options=['rock','scissors','paper'];
+    let computerChoice=Math.floor(Math.random()*3);
+    
+    return options[computerChoice];
+}
+
+function playRound(e){
+    let result;
+
+    let computerSelection = computerPlay();
+    let playerSelection = e.target.value;
+
+    if(playerSelection===computerSelection){
+        result=`${playerSelection} ties ${computerSelection}`;
+        tie++;
+    }else if((playerSelection==='rock' && computerSelection==='scissors') || (playerSelection==='scissors' && computerSelection==='paper') || (playerSelection==='paper' && computerSelection==='rock')){
+        result=`You Win!, ${playerSelection} beats ${computerSelection}`;
+        win++;
+    }else{
+        result = `You Lose!, ${computerSelection} beats ${playerSelection}`;
+        lose++;
+    }
+
+    let para = document.getElementById('result');
+    let paraWins = document.getElementById('win');
+    let paraLose = document.getElementById('lose');
+    let paraTies = document.getElementById('tie');
+
+    para.textContent=result;
+    paraWins.textContent = `Win: ${win}`;
+    paraLose.textContent = `Lose: ${lose}`;
+    paraTies.textContent = `Tie: ${tie}`;
+    
+    //return [result, win, lose, tie];
+}
+
+/*function game(){
     let win=0;
     let lose=0;
     let tie=0;
 
     while(win < 5 && lose < 5){
-        const playerSelection=prompt('abc').toLowerCase();
+        //const playerSelection=prompt('abc').toLowerCase();
         const computerSelection=computerPlay();
         let result=playRound(playerSelection,computerSelection);
 
@@ -21,32 +70,21 @@ function game(){
     }
 
     console.log(`The final result is:\nYou won ${win} times.\nYou lost ${lose} times.\nYou tied ${tie} times.`);
-}
+}*/
 
-function computerPlay(){
-    const options=['rock','scissors','paper'];
-    let computerChoice=Math.floor(Math.random()*3);
-    
-    return options[computerChoice];
-}
 
-function playRound(playerSelection,computerSelection){
-    let result;
-    let win;
-    let lose;
-    let tie;
 
-    if(playerSelection===computerSelection){
-        result=`${playerSelection} ties ${computerSelection}`;
-        tie=1;
-    }else if((playerSelection==='rock' && computerSelection==='scissors') || (playerSelection==='scissors' && computerSelection==='paper') || (playerSelection==='paper' && computerSelection==='rock')){
-        result=`You Win!, ${playerSelection} beats ${computerSelection}`;
-        win=1;
-    }else{
-        result = `You Lose!, ${computerSelection} beats ${playerSelection}`;
-        lose=1;
-    }
 
-    return [result, win, lose, tie];
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
