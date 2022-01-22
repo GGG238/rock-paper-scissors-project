5601479fb1ff95f242d318b94561be6abc57b845
Gilberto Garcia <GGG12022021@gmail.com>
2022-01-21 21:10:56 -0600

    Add CSS

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ for(let button of buttons){
 }
 
 
-
-
 function computerPlay(){
     const options=['rock','scissors','paper'];
     let computerChoice=Math.floor(Math.random()*3);
@@ -34,6 +32,30 @@ function playRound(e){
     }else{
         result = `You Lose!, ${computerSelection} beats ${playerSelection}`;
         lose++;
+    }
+
+    let playerChoise = document.getElementById('player');
+    let enemyChoise = document.getElementById('enemy');
+    let enemyText = document.getElementById('enemy-text');
+
+    playerChoise.style.backgroundImage="none";
+
+    if(playerSelection==='rock'){
+        playerChoise.style.cssText="background: url(./img/rock.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px";
+    }else if(playerSelection==='paper'){
+        playerChoise.style.cssText="background: url(./img/paper.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px";
+    }else if(playerSelection==='scissors'){
+        playerChoise.style.cssText="background: url(./img/scissors.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px";
+    }
+
+    enemyText.style.transform="scaleX(-1)";
+
+    if(computerSelection==='rock'){
+        enemyChoise.style.cssText="background: url(./img/rock.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px; transform: scaleX(-1);";
+    }else if(computerSelection==='paper'){
+        enemyChoise.style.cssText="background: url(./img/paper.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px; transform: scaleX(-1);";
+    }else if(computerSelection==='scissors'){
+        enemyChoise.style.cssText="background: url(./img/scissors.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px; transform: scaleX(-1);";
     }
 
     let para = document.getElementById('result');
