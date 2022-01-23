4bfb747474713fbce798f9dd46b5da97e7c57806
Gilberto Garcia <GGG12022021@gmail.com>
2022-01-22 19:14:46 -0600

    Finish the project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors-project
+
+Play rock paper scissors!!!
+
+All the images used in this project where downloaded from "The noun project" (https://thenounproject.com) and created by Cristiano Zoucas (https://thenounproject.com/cristiano.zoucas/).
--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-//game();
 let win=0;
 let lose=0;
 let tie=0;
 
 let buttons = Array.from(document.querySelectorAll("button"));
+let playAgain = document.createElement('div');
+let btn = document.createElement('button');
+
+let para = document.getElementById('result');
+let paraWins = document.getElementById('win');
+let paraLose = document.getElementById('lose');
+let paraTies = document.getElementById('tie');
+let displayResult = document.getElementById('display-result');
+let imgDisplay = document.getElementById('images');    
+
+playAgain.style.cssText = "margin: 24px 0; display: flex; width: 100%; justify-content: center; ";
+btn.style.cssText = " background: url(./img/play-again.png) no-repeat; background-size: 45%; background-position: center;"
+playAgain.append(btn);
 
 for(let button of buttons){
     button.addEventListener('click',playRound);
 }
+
+btn.addEventListener('click',Again);
 
 
 function computerPlay(){
@@ -58,55 +72,49 @@ function playRound(e){
         enemyChoise.style.cssText="background: url(./img/scissors.png) no-repeat; background-size: 40%; background-position: center; background-position-y: 70px; transform: scaleX(-1);";
     }
 
-    let para = document.getElementById('result');
-    let paraWins = document.getElementById('win');
-    let paraLose = document.getElementById('lose');
-    let paraTies = document.getElementById('tie');
+    if(win===5 || lose===5){
+        for(let button of buttons){
+            button.disabled=true;
+            button.style.cursor = 'default';
+        }
 
-    para.textContent=result;
+        displayResult.replaceChild(playAgain,imgDisplay);
+        enemyChoise.style.backgroundImage = "none";
+        playerChoise.style.backgroundImage = "none";
+
+        if(win===5){
+            para.textContent = "Congratulations!, you won!";
+        }else if(lose===5){
+            para.textContent = "Ouch!, you lost";
+        }
+
+    }else{
+        para.textContent=result;
+    }
+
     paraWins.textContent = `Win: ${win}`;
     paraLose.textContent = `Lose: ${lose}`;
     paraTies.textContent = `Tie: ${tie}`;
     
-    //return [result, win, lose, tie];
 }
 
-/*function game(){
-    let win=0;
-    let lose=0;
-    let tie=0;
+function Again(){
+    win=0;
+    lose=0;
+    tie=0;
 
-    while(win < 5 && lose < 5){
-        //const playerSelection=prompt('abc').toLowerCase();
-        const computerSelection=computerPlay();
-        let result=playRound(playerSelection,computerSelection);
+    para.textContent = '';
+    paraWins.textContent = `Win: ${win}`;
+    paraLose.textContent = `Lose: ${lose}`;
+    paraTies.textContent = `Tie: ${tie}`;
 
-        console.log(result[0]);
-        if(result[1]){
-            win++;
-        }else if(result[2]){
-            lose++;
-        }else if(result[3]){
-            tie++;
-        }
+    for(let button of buttons){
+        button.disabled=false;
+        button.style.cursor = 'pointer';
     }
 
-    console.log(`The final result is:\nYou won ${win} times.\nYou lost ${lose} times.\nYou tied ${tie} times.`);
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    displayResult.replaceChild(imgDisplay,playAgain);
+}
 
 
 
